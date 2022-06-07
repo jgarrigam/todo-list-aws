5e8218709571e1a7441dedd6b9720e76433dfdc5
Ubuntu <ubuntu@ip-172-31-93-200.ec2.internal>
2022-06-07 16:44:16 +0000

    se añade un espacio delante del pragma

--- src/todoList.py
+++ src/todoList.py
@@ -30,7 +30,7 @@ def get_item(key, dynamodb=None):
             }
         )
 
-    except ClientError as e: # pragma: no cover
+    except ClientError as e:  # pragma: no cover
         print(e.response['Error']['Message'])
     else:
         print('Result getItem:'+str(result))
@@ -65,7 +65,7 @@ def put_item(text, dynamodb=None):
             "body": json.dumps(item)
         }
 
-    except ClientError as e: # pragma: no cover
+    except ClientError as e:  # pragma: no cover
         print(e.response['Error']['Message'])
     else:
         return response
@@ -94,7 +94,7 @@ def update_item(key, text, checked, dynamodb=None):
             ReturnValues='ALL_NEW',
         )
 
-    except ClientError as e: # pragma: no cover
+    except ClientError as e:  # pragma: no cover
         print(e.response['Error']['Message'])
     else:
         return result['Attributes']
@@ -110,7 +110,7 @@ def delete_item(key, dynamodb=None):
             }
         )
 
-    except ClientError as e: # pragma: no cover
+    except ClientError as e:  # pragma: no cover
         print(e.response['Error']['Message'])
     else:
         return
