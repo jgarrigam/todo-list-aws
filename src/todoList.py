93e2b10ea34b724fbfabd7bf9c154c52e11ca454
jgarrigam <jgarriga@gmail.com>
2022-06-08 18:02:43 +0000

    + flake8 src/__init__.py src/create.py src/decimalencoder.py src/delete.py src/get.py src/list.py src/todoList.py src/update.py src/todoList.py:162:53: E251 unexpected spaces around keyword / parameter equals src/todoList.py:162:55: E251 unexpected spaces around keyword / parameter equals src/todoList.py:162:79: W291 trailing whitespace src/todoList.py:163:53: E127 continuation line over-indented for visual indent src/todoList.py:163:71: E251 unexpected spaces around keyword / parameter equals src/todoList.py:163:73: E251 unexpected spaces around keyword / parameter equals src/todoList.py:163:80: E501 line too long (80 > 79 characters) src/todoList.py:163:81: W291 trailing whitespace src/todoList.py:164:53: E127 continuation line over-indented for visual indent src/todoList.py:164:71: E251 unexpected spaces around keyword / parameter equals src/todoList.py:164:73: E251 unexpected spaces around keyword / parameter equals src/todoList.py:164:80: E501 line too long (82 > 79 characters)

--- src/todoList.py
+++ src/todoList.py
@@ -159,9 +159,9 @@ def get_translation(key, language, dynamodb=None):
                 'id': key
             }
         )
-        translation = translator.translate_text(Text = result['Item']["text"], 
-                                                    SourceLanguageCode = "auto", 
-                                                    TargetLanguageCode = language)
+        translation = translator.translate_text(Text=result['Item']["text"], 
+                                                SourceLanguageCode="auto", 
+                                                TargetLanguageCode=language)
     except ClientError as e:  # pragma: no cover
         print(e.response['Error']['Message'])
     else:
